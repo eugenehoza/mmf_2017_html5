56882a8d4429f01cb67cddf8701873050c76de71
MaximDov <31737353+MaximDov@users.noreply.github.com>
2017-10-23 03:43:19 +0300

    update on p6 homework

--- 06 express framework/homework/Dovydenko M/static-files/index.js
+++ 06 express framework/homework/Dovydenko M/static-files/index.js
@@ -2,26 +2,24 @@ var express = require("express");
 var path    = require('path');
 var app     = express();
 
+const mime = require('mime');
+
 // static working directory set up
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/sample.txt', function (req, res) {
-  res.set('Content-Type', 'text/plain');
-})
-app.get('/sample.docx', function (req, res) {
-  res.set('Content-Type', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document');
-})
-app.get('/brown.pdf', function (req, res) {
-  res.set('Content-Type', 'application/pdf');
+app.get('/*', function (req, res) {
+  res.set('Content-Type', mime.lookup(path));
 })
 
-// понимаю, что параметр запроса Content-Type нужно обрабатывать
-// по имени любого запрашиваемого файла, но мне пока не удается
-// обратиться к req.path элементу
+// вызов, который предположительно должен обрабатывать любой запрашиваемый
+// файл (путь) в static директории, но что с ним, что без него функционал
+// остается тем же - запрашиваемый файл просто скачивается/ открывается
+// в браузере.
 
 app.listen(3000);
 console.log('Server running at localhost:3000');
 console.log('try GET:');
 console.log('         sample.txt');
 console.log('         sample.docx');
-console.log('         brown.pdf');
+console.log('         sub/sample.xlsx');
+console.log('         sub/brown.pdf');
