8a447fbaf61cf6dc587d58acd08eff6cd5f00269
Eugene Hoza <eugenegoza@gmail.com>
2017-11-01 01:36:32 +0300

    (hoza) Added lecture 8 results

---
+++ 08 db/lecture/server.js
@@ -0,0 +1,87 @@
+var express = require('express')
+var app = express()
+var path    = require("path");
+var sqlite3 = require('sqlite3').verbose();
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/number', function (req, res) {
+  res.send('4');
+})
+
+app.get('/index.html', function (req, res) {
+   res.sendFile(path.join(__dirname+'/index.html'));
+})
+
+let db;
+
+app.get('/createSchemaInMemory', (req, res) => {
+  db = new sqlite3.Database(':memory:', (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
+  });
+
+  db.run('CREATE TABLE users(id number, name text, email text)', (err) => {
+    if (err) {
+      return console.error(err.message);
+    }});
+
+  console.log('Created table users');
+
+  res.send('ok');
+});
+
+app.get('/fillInMemory', (req, res) => {
+  db.exec('insert into users values(1, \'test10\', \'test10@email.com\')');
+  db.exec('insert into users values(2, \'test20\', \'test20@email.com\')');
+  res.send('ok');
+});
+
+app.get('/readfromMemory', (req, res) => {
+  let sql = `SELECT *
+            FROM users
+            ORDER BY name`;
+  var data = [];
+  db.each(sql, (err, row) => {
+    if (err) {
+      throw err;
+    }
+    console.log('Item '+JSON.stringify(row));
+    data.push(row)
+  }, () => {
+    console.log('Data '+JSON.stringify(data));
+    res.send(data);
+  });
+});
+
+app.get('/readFromTest', (req, res) => {
+  let db = new sqlite3.Database(__dirname + '/test.db');
+
+  let sql = `SELECT *
+            FROM users
+            ORDER BY name`;
+
+  var data = [];
+
+  db.each(sql, (err, row) => {
+    if (err) {
+      throw err;
+    }
+    console.log('Item '+JSON.stringify(row));
+    data.push(row)
+  }, () => {
+    db.close();
+    console.log('Data '+JSON.stringify(data));
+    res.send(data);
+  });
+
+
+});
+
+app.listen(3000);
+
+console.log("Running at Port 3000");
