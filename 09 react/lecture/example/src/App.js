0db5f20c8867492f49ea5673de3f0361e8304dd6
Eugene Hoza <eugenegoza@gmail.com>
2017-11-15 00:48:02 +0300

    (hoza) Added react example for lecture

---
+++ 09 react/lecture/example/src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Menu, MenuItem} from 'react-foundation';
+
+const Test = () => (
+  <div>
+    Test page for react-router
+  </div>
+)
+
+const Header = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+    </header>
+  </div>
+)
+
+const Home = () => (
+  <p>
+    Welcome from start page
+  </p>
+)
+
+const Nav = () => (
+  <div>
+    <Menu>
+      <MenuItem><a href="/">Home</a></MenuItem>
+      <MenuItem><a href="/test">Test</a></MenuItem>
+    </Menu>
+  </div>
+)
+
+class App extends Component {
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <Header/>
+          <Nav/>
+          <Route exact path="/" component={Home}/>
+          <Route path="/test" component={Test}/>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
