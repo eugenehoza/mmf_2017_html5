bcf3c5788140a78805c152e744eb5e7bfe7bfd5f
Eugene Hoza <eugenegoza@gmail.com>
2017-12-05 23:45:58 +0300

    (hoza) Added lab results

--- questions/#start.bat
+++ questions/#start.bat
@@ -1,1 +1,11 @@
-node questgen.js questions_example.txt
+node questgen.js questions_example1.txt
+node questgen.js questions_example2.txt
+node questgen.js questions_example3.txt
+node questgen.js questions_example4.txt
+node questgen.js questions_example5.txt
+node questgen.js questions_example6.txt
+node questgen.js questions_example7.txt
+node questgen.js questions_example8.txt
+node questgen.js questions_example9.txt
+node questgen.js questions_examplea.txt
+node questgen.js questions_exampleb.txt
