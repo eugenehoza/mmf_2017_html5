70ebecbcb40bb206a72e9e8dca01b10c167af0d9
KateKalina <kalinakat9@gmail.com>
2017-12-13 00:14:12 +0300

    Manifest
    Added manifest.

---
+++ 07 standalone/homework/Kalina K/params/index.js
@@ -0,0 +1,84 @@
+/**
+ * Module dependencies.
+ */
+var express = require('express');
+var app = module.exports = express();
+
+var c = require('appcache-node');
+// generate a cache file
+var cf = c.newCache(['../']);
+// Faux database
+
+var users = [
+    { name: 'tj' }
+  , { name: 'tobi' }
+  , { name: 'loki' }
+  , { name: 'jane' }
+  , { name: 'bandit' }
+];
+
+// Create HTTP error
+
+function createError(status, message) {
+  var err = new Error(message);
+  err.status = status;
+  return err;
+}
+
+// Convert :to and :from to integers
+
+app.param(['to', 'from'], function(req, res, next, num, name){
+  req.params[name] = parseInt(num, 10);
+  if( isNaN(req.params[name]) ){
+    next(createError(400, 'failed to parseInt '+num));
+  } else {
+    next();
+  }
+});
+
+// Load user by id
+
+app.param('user', function(req, res, next, id){
+  if (req.user = users[id]) {
+    next();
+  } else {
+    next(createError(404, 'failed to find user'));
+  }
+});
+
+/**
+ * GET index.
+ */
+ app.all('/app.cache', function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/cache-manifest'});
+    res.end(cf);
+});
+app.get('/', function(req, res){
+  res.send('Visit /user/0 or /users/0-2 or /users/<2');
+});
+
+/**
+ * GET :user.
+ */
+
+app.get('/user/:user', function(req, res, next){
+  res.send('user ' + req.user.name);
+});
+
+app.get('/users/'+encodeURIComponent('<') +':to', function(req, res, next){
+  var from = req.params.from;
+  var to = req.params.to;
+  var names = users.map(function(user) { return user.name; });
+  res.send('users ' + names.slice(0, to-1).join(', '));
+});
+app.get('/users/'+ encodeURIComponent('>')+':to', function(req, res, next){
+  var from = req.params.from;
+  var to = req.params.to;
+  var names = users.map(function(user){ return user.name; });
+  res.send('users ' + names.slice(to, users.length).join(', '));
+});
+/* istanbul ignore next */
+if (!module.parent) {
+  app.listen(3000);
+  console.log('Express started on port 3000');
+}
